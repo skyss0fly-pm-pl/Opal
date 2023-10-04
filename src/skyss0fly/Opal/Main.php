226a2adf8dedc2b74260ec55102fe18e2d88b7d0
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2023-10-04 17:17:32 +1000

    Update Main.php

--- src/skyss0fly/Opal/Main.php
+++ src/skyss0fly/Opal/Main.php
@@ -4,23 +4,13 @@ use pocketmine\player\Player;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
-
-
 class Main extends PluginBase {
   public function onEnable(): void {
     $this->getLogger()->info(TEXTFORMAT::BOLD . TEXTFORMAT::YELLOW . "[" .TEXTFORMAT::RED . "OPAL" . TEXTFORMAT::YELLOW . "]" . TEXTFORMAT::GREEN . " Opal is ready to go!");
   }
  public function onChat(PlayerChatEvent $event) {
- $rand = rand(1,5); 
-  if ($rand === 4) {
     $strings = array( 'What?', 'Yesterday was Cheese Day!', "A potato flew around my room", "hold on guys, gotta walk my fish rq", "ok 19 dollar fortnite card", "when?", "Crazy? i was crazy once", "Did somebody say Menulog?", "Hey baba grill", "ok who moved my secret stash?", "guys my fish just invented a time machine", "cap", "when the imposter is sus", "GOD JUST SHUT UP!", "UwU", "change your underwear bro, i can smell them thru the screen" ); 
     $key = array_rand($strings); 
-    $this->getServer()->broadcastMessage(TEXTFORMAT::BOLD . TEXTFORMAT::YELLOW . "[" .TEXTFORMAT::RED . "OPAL" . TEXTFORMAT::YELLOW . "] " . TEXTFORMAT::GREEN . $strings[$key]);
-  }
-  else {
-    /*nothing
-      lol
-      */
-  }
+    $this->getServer()->broadcastMessage(TEXTFORMAT::BOLD . TEXTFORMAT::YELLOW . "[" .TEXTFORMAT::RED . "OPAL" . TEXTFORMAT::YELLOW . "] " . TEXTFORMAT::GREEN . $strings[$key]);  
 }
 }
